d739c5b57785179d277535e0e801867dc1171fb8
Austin O'Hara <austinaohara@gmail.com>
2026-04-19 18:46:39 -0400

    created CoffeeBuilder and LatteBuilder

--- src/main/java/edu/farmingdale/ramsbrew/builder/CoffeeBuilder.java
+++ src/main/java/edu/farmingdale/ramsbrew/builder/CoffeeBuilder.java
@@ -1,4 +1,12 @@
 package edu.farmingdale.ramsbrew.builder;
 
-public class CoffeeBuilder {
+import edu.farmingdale.ramsbrew.beverage.Beverage;
+import edu.farmingdale.ramsbrew.beverage.Coffee;
+
+public class CoffeeBuilder extends AbstractBeverageBuilder {
+    @Override
+    public Beverage build() {
+        requireNoShots();
+        return new Coffee(size, temperature, milkType, shots, sweetenerType);
+    }
 }
