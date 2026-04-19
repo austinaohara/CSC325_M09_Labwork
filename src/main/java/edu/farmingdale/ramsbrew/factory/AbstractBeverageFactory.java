eae9b1522dd09430da7c8efaced25911cf8857b6
Austin O'Hara <austinaohara@gmail.com>
2026-04-19 18:50:28 -0400

    created AbstractBeverageFactory

--- src/main/java/edu/farmingdale/ramsbrew/factory/AbstractBeverageFactory.java
+++ src/main/java/edu/farmingdale/ramsbrew/factory/AbstractBeverageFactory.java
@@ -1,4 +1,13 @@
 package edu.farmingdale.ramsbrew.factory;
 
+import edu.farmingdale.ramsbrew.beverage.Beverage;
+import edu.farmingdale.ramsbrew.builder.BeverageBuilder;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
 public interface AbstractBeverageFactory {
+    String getFactoryName();
+
+    BeverageBuilder createBuilder();
+
+    Beverage createDefaultBeverage(Temperature temperature);
 }
