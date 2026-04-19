3470f425a636f03d7105da7404210cb97ca29087
Kelvin Morales <kelvin.morales906@gmail.com>
2026-04-19 18:39:16 -0400

    created cappuccino and tea

--- src/main/java/edu/farmingdale/ramsbrew/beverage/Cappuccino.java
+++ src/main/java/edu/farmingdale/ramsbrew/beverage/Cappuccino.java
@@ -1,4 +1,22 @@
 package edu.farmingdale.ramsbrew.beverage;
 
-public class Cappuccino {
+import edu.farmingdale.ramsbrew.model.MilkType;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.SweetenerType;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
+public class Cappuccino extends BaseBeverage {
+    protected Cappuccino(Size size, Temperature temperature, MilkType milkType, int shots, SweetenerType sweetenerType) {
+        super(size, temperature, milkType, shots, sweetenerType);
+    }
+
+    @Override
+    public String getName() {
+        return "Cappuccino";
+    }
+
+    @Override
+    public double getPrice() {
+        return 4.50 + sizeUpcharge() + milkUpcharge() + shotUpcharge();
+    }
 }
--- src/main/java/edu/farmingdale/ramsbrew/beverage/Tea.java
+++ src/main/java/edu/farmingdale/ramsbrew/beverage/Tea.java
@@ -1,4 +1,22 @@
 package edu.farmingdale.ramsbrew.beverage;
 
-public class Tea {
+import edu.farmingdale.ramsbrew.model.MilkType;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.SweetenerType;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
+public class Tea extends BaseBeverage{
+    protected Tea(Size size, Temperature temperature, MilkType milkType, int shots, SweetenerType sweetenerType) {
+        super(size, temperature, milkType, shots, sweetenerType);
+    }
+
+    @Override
+    public String getName() {
+        return "Tea";
+    }
+
+    @Override
+    public double getPrice() {
+        return 2.25 + sizeUpcharge() + milkUpcharge();
+    }
 }
