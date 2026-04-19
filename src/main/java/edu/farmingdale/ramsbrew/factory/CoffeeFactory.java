ad89cec9e05bb8a959ac8d9424a9eeeae4f3d7ff
Austin O'Hara <austinaohara@gmail.com>
2026-04-19 18:51:03 -0400

    created CoffeeFactory and LatteFactory

--- src/main/java/edu/farmingdale/ramsbrew/factory/CoffeeFactory.java
+++ src/main/java/edu/farmingdale/ramsbrew/factory/CoffeeFactory.java
@@ -1,4 +1,27 @@
 package edu.farmingdale.ramsbrew.factory;
 
-public class CoffeeFactory {
+import edu.farmingdale.ramsbrew.beverage.Beverage;
+import edu.farmingdale.ramsbrew.builder.BeverageBuilder;
+import edu.farmingdale.ramsbrew.builder.CoffeeBuilder;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
+public class CoffeeFactory implements AbstractBeverageFactory {
+    @Override
+    public String getFactoryName() {
+        return "Coffee Factory";
+    }
+
+    @Override
+    public BeverageBuilder createBuilder() {
+        return new CoffeeBuilder();
+    }
+
+    @Override
+    public Beverage createDefaultBeverage(Temperature temperature) {
+        return createBuilder()
+                .setSize(Size.MEDIUM)
+                .setTemperature(temperature)
+                .build();
+    }
 }
--- src/main/java/edu/farmingdale/ramsbrew/factory/LatteFactory.java
+++ src/main/java/edu/farmingdale/ramsbrew/factory/LatteFactory.java
@@ -1,4 +1,27 @@
 package edu.farmingdale.ramsbrew.factory;
 
-public class LatteFactory {
+import edu.farmingdale.ramsbrew.beverage.Beverage;
+import edu.farmingdale.ramsbrew.builder.BeverageBuilder;
+import edu.farmingdale.ramsbrew.builder.LatteBuilder;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
+public class LatteFactory implements AbstractBeverageFactory {
+    @Override
+    public String getFactoryName() {
+        return "Latte Factory";
+    }
+
+    @Override
+    public BeverageBuilder createBuilder() {
+        return new LatteBuilder();
+    }
+
+    @Override
+    public Beverage createDefaultBeverage(Temperature temperature) {
+        return createBuilder()
+                .setSize(Size.MEDIUM)
+                .setTemperature(temperature)
+                .build();
+    }
 }
