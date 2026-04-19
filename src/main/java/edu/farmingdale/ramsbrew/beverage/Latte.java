ac7053c49b807cc7c6ca6ad312d4b4099408c511
Austin O'Hara <austinaohara@gmail.com>
2026-04-19 18:34:51 -0400

    created coffee and latte

--- src/main/java/edu/farmingdale/ramsbrew/beverage/Latte.java
+++ src/main/java/edu/farmingdale/ramsbrew/beverage/Latte.java
@@ -1,4 +1,22 @@
 package edu.farmingdale.ramsbrew.beverage;
 
-public class Latte {
+import edu.farmingdale.ramsbrew.model.MilkType;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.SweetenerType;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
+public class Latte extends BaseBeverage {
+    public Latte(Size size, Temperature temperature, MilkType milkType, int shots, SweetenerType sweetenerType) {
+        super(size, temperature, milkType, shots, sweetenerType);
+    }
+
+    @Override
+    public String getName() {
+        return "Latte";
+    }
+
+    @Override
+    public double getPrice() {
+        return 4.25 + sizeUpcharge() + milkUpcharge() + shotUpcharge();
+    }
 }
