88e739da46e181dac8c399e2e20388f232175cde
Kelvin Morales <kelvin.morales906@gmail.com>
2026-04-19 18:45:12 -0400

    created AbstractBeverageBuilder and BeverageBuilder

--- src/main/java/edu/farmingdale/ramsbrew/builder/AbstractBeverageBuilder.java
+++ src/main/java/edu/farmingdale/ramsbrew/builder/AbstractBeverageBuilder.java
@@ -1,4 +1,56 @@
 package edu.farmingdale.ramsbrew.builder;
 
-public abstract class AbstractBeverageBuilder {
+import edu.farmingdale.ramsbrew.model.MilkType;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.SweetenerType;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
+public abstract class AbstractBeverageBuilder implements BeverageBuilder {
+    protected Size size = Size.MEDIUM;
+    protected Temperature temperature = Temperature.HOT;
+    protected MilkType milkType = MilkType.NONE;
+    protected int shots = 0;
+    protected SweetenerType sweetenerType = SweetenerType.NONE;
+
+    @Override
+    public BeverageBuilder setSize(Size size) {
+        this.size = size;
+        return this;
+    }
+
+    @Override
+    public BeverageBuilder setTemperature(Temperature temperature) {
+        this.temperature = temperature;
+        return this;
+    }
+
+    @Override
+    public BeverageBuilder setMilkType(MilkType milkType) {
+        this.milkType = milkType;
+        return this;
+    }
+
+    @Override
+    public BeverageBuilder setShots(int shots) {
+        this.shots = shots;
+        return this;
+    }
+
+    @Override
+    public BeverageBuilder setSweetenerType(SweetenerType sweetenerType) {
+        this.sweetenerType = sweetenerType;
+        return this;
+    }
+
+    protected void requireShotRangeForEspressoDrinks() {
+        if (shots < 1 || shots > 3) {
+            throw new IllegalArgumentException("Espresso drinks require 1 to 3 shots.");
+        }
+    }
+
+    protected void requireNoShots() {
+        if (shots != 0) {
+            throw new IllegalArgumentException("This beverage does not support espresso shots.");
+        }
+    }
 }
--- src/main/java/edu/farmingdale/ramsbrew/builder/BeverageBuilder.java
+++ src/main/java/edu/farmingdale/ramsbrew/builder/BeverageBuilder.java
@@ -1,4 +1,21 @@
 package edu.farmingdale.ramsbrew.builder;
 
+import edu.farmingdale.ramsbrew.beverage.Beverage;
+import edu.farmingdale.ramsbrew.model.MilkType;
+import edu.farmingdale.ramsbrew.model.Size;
+import edu.farmingdale.ramsbrew.model.SweetenerType;
+import edu.farmingdale.ramsbrew.model.Temperature;
+
 public interface BeverageBuilder {
+    BeverageBuilder setSize(Size size);
+
+    BeverageBuilder setTemperature(Temperature temperature);
+
+    BeverageBuilder setMilkType(MilkType milkType);
+
+    BeverageBuilder setShots(int shots);
+
+    BeverageBuilder setSweetenerType(SweetenerType sweetenerType);
+
+    Beverage build();
 }
